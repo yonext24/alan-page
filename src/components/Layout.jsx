e3f93c1ebde72a7f389f9762a6f3fc6fa5dba0c1
Yanzz <yonielkpo@gmail.com>
2023-02-14 19:02:08 -0300

    Added correct camel case to run build -.-

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,8 @@
 import { Navbar } from './Navbar'
 import styles from '@/styles/layout.module.css'
-import { Fira_Sans } from '@next/font/google'
+import { Fira_Sans as FiraSans } from '@next/font/google'
 
-const prompt = Fira_Sans({
+const prompt = FiraSans({
   weight: ['400', '700'],
   subsets: ['latin'],
   fallback: ['system-ui', 'arial'],
